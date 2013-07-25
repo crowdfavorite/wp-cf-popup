ec6772fe253ee6c0e526bcc8163aacd0d2ec0452
Matt Richmond <matt@crowdfavorite.com>
2013-07-25 13:40:26 -0600

    memory crashes on iOS devices, so don't run on those

--- js/cf-popup.js
+++ js/cf-popup.js
@@ -1,4 +1,8 @@
 (function($){
+	// Don't run this on iOS. It appears to crash these devices running Mobile Safari currently.
+	if (navigator.userAgent.match(/[\W]i(pad|phone|pod)[\W]/i)) {
+	        return;
+	}
 	cfPopup = {
 		showWhen : _cfPopup.showWhen,
 		interval : parseInt(_cfPopup.interval, 10), // in days
