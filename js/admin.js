94a536e3d9dcf35b9bb93721eb4c9dfac42951bd
Matt Richmond <matt@crowdfavorite.com>
2013-08-01 00:57:38 -0600

    only operate (show/hide) on the first parent <tr> it comes across

--- js/admin.js
+++ js/admin.js
@@ -5,17 +5,27 @@
 			cfPopupAdmin.showWhenDropdown = $('#js_cf_popup_settings__show_when');
 			cfPopupAdmin.waitTimeElem = $('#js_cf_popup_settings__wait_time');
 		},
+		hideRow : function(elem) {
+			elem.parents('tr').first().hide();
+		},
+		showRow : function(elem) {
+			elem.parents('tr').first().show();
+		},
 		hideWaitTime : function() {
-			cfPopupAdmin.waitTimeElem.parents('tr').hide();
+			cfPopupAdmin.hideRow(cfPopupAdmin.waitTimeElem);
 		},
 		showWaitTime : function() {
-			cfPopupAdmin.waitTimeElem.parents('tr').show();
+			cfPopupAdmin.showRow(cfPopupAdmin.waitTimeElem);
 		},
 		hideAllRemainingInputs : function() {
-			$('.js_hide_on_never').parents('tr').hide();
+			$('.js_hide_on_never').each(function(index) {
+				cfPopupAdmin.hideRow($(this));
+			});
 		},
 		showAllRemainingInputs : function() {
-			$('.js_hide_on_never').parents('tr').show();
+			$('.js_hide_on_never').each(function(index) {
+				cfPopupAdmin.showRow($(this));
+			});
 		},
 		showHideElements : function() {
 			console.log(cfPopupAdmin.showWhenDropdown);
